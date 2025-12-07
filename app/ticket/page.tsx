edcd570569a60680f50be0a16913fb6f0e2a5b0d
Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2025-12-07 15:03:04 +0300

    modified files

---
+++ app/ticket/page.tsx
@@ -0,0 +1,10 @@
+function TicketPage() {
+  return (
+    <main style={{ margin: "0 auto", padding: "4", maxWidth: "800px", textAlign: "center" }}>
+      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      <p>This is the main landing page of the ticket application.</p>
+    </main>
+  );
+}
+
+export default TicketPage;
